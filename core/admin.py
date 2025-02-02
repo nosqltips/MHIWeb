bbcebaa717ffa27634a737d51196f03f26b2237d
Craig Brown <cbrown@nosqlrevolution.com>
2025-02-02 16:37:30 -0700

    Add models to admin. Add venv.

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User, Group
 from MHIWeb.admin import admin_site  # Import your custom admin site
+from .models import MhiPerson, MhiLuAssignment, MhiLuRole, MhiLanguage, MhiAreaWard, MhiAssignmentRole
+from .models import MhiPersonAssignmentRole, MhiPersonContact, MhiPersonLanguage, MhiProcessTracking, MhiViewPersonToSpreadsheet
 
 # Register models with your custom admin site
 admin_site.register(User, UserAdmin)
@@ -10,3 +12,14 @@ admin_site.register(Group)
 # Register your own models here
 # from .models import YourModel
 # admin_site.register(YourModel)
+admin_site.register(MhiPerson)
+admin_site.register(MhiLuAssignment)
+admin_site.register(MhiLuRole)
+admin_site.register(MhiLanguage)
+admin_site.register(MhiAreaWard)
+admin_site.register(MhiAssignmentRole)
+admin_site.register(MhiPersonAssignmentRole)
+admin_site.register(MhiPersonContact)
+admin_site.register(MhiPersonLanguage)
+admin_site.register(MhiProcessTracking)
+admin_site.register(MhiViewPersonToSpreadsheet)
---
+++ venv/bin/django-crud-generator.py
@@ -0,0 +1,5 @@
+#!/Users/cbrown/projects/MHIWeb/venv/bin/python3.12
+from django_crud_generator import execute_from_command_line
+
+if __name__ == "__main__":
+    execute_from_command_line()
